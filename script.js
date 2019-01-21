01642aae051ecd333b0ae5aae30dd98f0de33a9e
march <francois.cousin62@orange.fr>
2019-01-21 08:18:51 +0100

    first work

---
+++ script.js
@@ -0,0 +1,121 @@
+/* 
+
+	4. Init Progress Bars
+
+	*/
+
+	function initProgressBars()
+	{
+		if($('.skill_bars').length)
+		{
+			var eles = $('.skill_bars');
+			var gradNum = 0;
+
+			eles.each(function(i)
+			{
+
+				var ele = $(this);
+	    		var elePerc = ele.data('perc');
+	    		var eleName = '#'+ele.data('name');
+	    		var eleColorStart = ele.data('color-start');
+	    		var eleColorEnd = ele.data('color-end');
+	    		var eleGradient = 'url(#gradient' + gradNum.toString() +')';
+	    		let linearGradient = '<defs><linearGradient id="gradient' + gradNum + '" x1="0%" y1="0%" x2="100%" y2="0%" gradientUnits="userSpaceOnUse"><stop offset="20%" stop-color="'+ eleColorStart +'"/><stop offset="50%" stop-color="'+ eleColorEnd+'"/></linearGradient></defs>';
+	    		
+	    		var statsScene = new ScrollMagic.Scene({
+		    		triggerElement: this,
+		    		triggerHook: 'onEnter',
+		    		reverse:false
+		    	})
+		    	.on('start', function()
+		    	{
+		    		var pbar = new ProgressBar.Line(eleName, 
+		    		{
+		    			strokeWidth: 1,
+						easing: 'easeInOut',
+						duration: 1400,
+						color: eleGradient,
+						trailColor: '#ffffff',
+						trailWidth: 1,
+						svgStyle: {width: '100%', height: '100%'},
+						text: {
+							style: {
+								position: 'absolute',
+								right: '0',
+								top: '-20px',
+								padding: 0,
+								margin: 0,
+								transform: null
+								},
+								autoStyleContainer: false
+						},
+						step: (state, bar) => {
+						bar.setText(Math.round(bar.value() * 100) + ' %');
+						}
+		    		});
+		    		pbar.animate(elePerc);
+					pbar.svg.insertAdjacentHTML('afterBegin', linearGradient);
+		    	})
+		    	.addTo(ctrl);
+		    	gradNum++;
+			});
+		}	
+	}
+
+	/* 
+
+	5. Initialize Milestones
+
+	*/
+
+	function initMilestones()
+	{
+		if($('.milestone_counter').length)
+		{
+			var milestoneItems = $('.milestone_counter');
+
+	    	milestoneItems.each(function(i)
+	    	{
+	    		var ele = $(this);
+	    		var endValue = ele.data('end-value');
+	    		var eleValue = ele.text();
+
+	    		/* Use data-sign-before and data-sign-after to add signs
+	    		infront or behind the counter number (+, k, etc) */
+	    		var signBefore = "";
+	    		var signAfter = "";
+
+	    		if(ele.attr('data-sign-before'))
+	    		{
+	    			signBefore = ele.attr('data-sign-before');
+	    		}
+
+	    		if(ele.attr('data-sign-after'))
+	    		{
+	    			signAfter = ele.attr('data-sign-after');
+	    		}
+
+	    		var milestoneScene = new ScrollMagic.Scene({
+		    		triggerElement: this,
+		    		triggerHook: 'onEnter',
+		    		reverse:false
+		    	})
+		    	.on('start', function()
+		    	{
+		    		var counter = {value:eleValue};
+		    		var counterTween = TweenMax.to(counter, 4,
+		    		{
+		    			value: endValue,
+		    			roundProps:"value", 
+						ease: Circ.easeOut, 
+						onUpdate:function()
+						{
+							document.getElementsByClassName('milestone_counter')[i].innerHTML = signBefore + counter.value + signAfter;
+						}
+		    		});
+		    	})
+			    .addTo(ctrl);
+	    	});
+		}
+	}
+
