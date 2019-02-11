ed565db6ab293a8212b56b3b3bce3d5d8451d324
march <francois.cousin62@orange.fr>
2019-02-11 15:37:54 +0100

    nouveaux rajouts

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// intro
+
+$(function(){
+    var welcomeSection = $('.welcome-section'),
+    enterButton= welcomeSection.find('.enter-button');
+
+    setTimeout(function(){
+        welcomeSection.removeClass('content-hidden');
+    },800);
+    enterButton.on('click', function(e){
+        e.preventDefault();
+        welcomeSection.addClass('content-hidden').fadeOut();
+    })
+})
+
+// bar de progression
+
 $(document).ready(function() {
 	$('.progress .progress-bar').css("width",
 			  function() {
