909dd673a7af96a2927f389ecbc4203f7ef9c68f
march <francois.cousin62@orange.fr>
2019-03-05 11:48:21 +0100

    modif de texte

--- script.js
+++ script.js
@@ -13,60 +13,6 @@ $(function(){
     })
 })
 
-/************************* accueil ****************************************/
-
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
-
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-    }
-
-    setTimeout(function() {
-    that.tick();
-    }, delta);
-};
-
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-};
-
 /************************* Compétences ****************************************/
 
 // bar de progression
@@ -88,3 +34,13 @@ $(function(){
        $('#info').text("Cette fonctionnalité évoluera avec ma formation. Merci de votre compréhension.")
     })
 })
+
+/************************* accueil ****************************************/
+
+var accueil = "Développeur web et web mobile. J\'aime imaginer et concevoir des sites internet. Ensemble faisons d\'internet un endroit merveilleux.";
+var str = accueil.split("");
+var el = document.getElementById('str');
+(function animate() {
+str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(run); 
+var run = setTimeout(animate, 90);
+})();
